d28777561e62a6c1640156910cd9893917706453
OiSiS <oisisk@gmail.com>
2026-02-21 04:28:18 +0100

    fix(backend): add missing corev1 and resource imports to resources.go

--- backend/handlers/resources.go
+++ backend/handlers/resources.go
@@ -7,6 +7,8 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
