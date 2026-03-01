07b5151981a6c9fd2e59744bf6a256275d1e3678
OiSiS <oisisk@gmail.com>
2026-03-01 19:06:50 +0100

    style: format Service list columns and add missing translations for all locales

--- web/src/components/ResourceList/templates/ServiceList.jsx
+++ web/src/components/ResourceList/templates/ServiceList.jsx
@@ -5,9 +5,9 @@ export const ServiceListSchema = {
         { key: 'namespace', label: 'Namespace' },
         { key: 'extra.labels', label: 'Labels' },
         { key: 'status', label: 'Type', badge: true },
-        { key: 'extra.cluster-ip', label: 'Cluster IP' },
-        { key: 'extra.endpoints', label: 'Int Endpoints' },
-        { key: 'extra.external', label: 'Ext Endpoints' },
+        { key: 'extra.cluster-ip', label: 'cluster ip' },
+        { key: 'extra.endpoints', label: 'int endpoints' },
+        { key: 'extra.external', label: 'ext endpoints' },
         { key: 'age', label: 'Created' },
     ],
 };
