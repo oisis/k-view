3768e921365187377057a7b43d78036ba2e454be
OiSiS <oisisk@gmail.com>
2026-03-05 12:05:11 +0100

    fix(backend): resolve missing images and pod metrics in ReplicaSet view

--- backend/handlers/mgr_replicaset.go
+++ backend/handlers/mgr_replicaset.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/gin-gonic/gin"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
@@ -61,5 +62,50 @@ func (m *ReplicaSetManager) MapItem(item unstructured.Unstructured, metricsMap m
 }
 
 func (m *ReplicaSetManager) GetDetails(ctx context.Context, dynClient dynamic.Interface, item unstructured.Unstructured) (gin.H, error) {
-	return m.GenericManager.GetDetails(ctx, dynClient, item)
+	response, err := m.GenericManager.GetDetails(ctx, dynClient, item)
+	if err != nil {
+		return nil, err
+	}
+
+	// Enhance response with specialized extra fields (including images)
+	mapped := m.MapItem(item, nil)
+	if extra, ok := response["extra"].(map[string]interface{}); ok {
+		for k, v := range mapped.Extra {
+			extra[k] = v
+		}
+	}
+
+	// Fetch metrics for related Pods using the dynamic client
+	metricsGVR := schema.GroupVersionResource{
+		Group:    "metrics.k8s.io",
+		Version:  "v1beta1",
+		Resource: "pods",
+	}
+	var metricsMap map[string]unstructured.Unstructured
+	mList, err := dynClient.Resource(metricsGVR).Namespace(item.GetNamespace()).List(ctx, metav1.ListOptions{})
+	if err == nil {
+		metricsMap = make(map[string]unstructured.Unstructured)
+		for _, m := range mList.Items {
+			key := m.GetNamespace() + "/" + m.GetName()
+			metricsMap[key] = m
+		}
+	}
+
+	// Fetch related Pods
+	podMgr := NewPodManager()
+	pods, err := dynClient.Resource(podMgr.GetGVR()).Namespace(item.GetNamespace()).List(ctx, metav1.ListOptions{})
+	if err == nil {
+		var relatedPods []ResourceItem
+		for _, pod := range pods.Items {
+			for _, owner := range pod.GetOwnerReferences() {
+				if owner.UID == item.GetUID() {
+					relatedPods = append(relatedPods, podMgr.MapItem(pod, metricsMap))
+					break
+				}
+			}
+		}
+		response["relatedPods"] = relatedPods
+	}
+
+	return response, nil
 }
